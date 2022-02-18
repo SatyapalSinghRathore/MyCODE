2c02238886388e3e05a6ecc6282e9cc91beb996c
SatyapalSinghRathore <84779103+SatyapalSinghRathore@users.noreply.github.com>
2022-02-18 16:30:16 +0530

    Add files via upload

---
+++ myC++/new.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+int main() {
+    for(int j=5; j>=0; j--) {
+        if (j==20 || j==0) {
+            continue;
+        }
+        cout<<"--Satyapal--\n";
+
+    }
+}
---
+++ myC++/text.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void loop(int n){
+    for (int i = 0; i < n+1; i++)
+    {
+      printf("\nhelo satya\n");
+    }
+    
+}
+void main() {
+    loop(5);
+}
